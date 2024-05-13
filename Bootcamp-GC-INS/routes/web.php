8fde3560958abfc6534234da6022727e66b5ae44
rahmathidaya13 <rahmathidaya1305@gmail.com>
2024-05-13 20:30:56 +0700

    Rahmat Hidaya: Update Laravel Bootcamp GC-INS

--- Bootcamp-GC-INS/routes/web.php
+++ Bootcamp-GC-INS/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/register', [AuthController::class, 'index'])->name('register');
 Route::post('/welcome', [AuthController::class, 'register'])->name('register.submit');
 
